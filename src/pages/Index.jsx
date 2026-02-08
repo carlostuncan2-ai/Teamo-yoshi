bfafbe71164c0cba0e033bf24edc28a0a916efef
carlostuncan2-ai <carlostuncan2@gmail.com>
2026-02-07 21:08:41 -0600

    Create Index page component

---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import FloatingHearts from '@/components/FloatingHearts';
+import ValentineCard from '@/components/ValentineCard';
+
+const Index = () => {
+    return (
+        <div className="min-h-screen bg-background relative overflow-hidden">
+            <FloatingHearts />
+            <ValentineCard />
+        </div>
+    );
+};
+
+export default Index;
